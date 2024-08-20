ca353fee60116149960746d7d33c55a32c781512
Slauva <koshman2015@list.ru>
2024-08-20 23:50:37 +0300

    ci: setup the docker settings

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import withNextIntl from "next-intl/plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone", // Docker container optimization
-
 };
 
 export default withNextIntl("./src/shared/lib/i18n.ts")(nextConfig);
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,19 @@ import { NextRequest, NextResponse } from "next/server";
 export const config = {
   matcher: [
     // Enable a redirect to a matching locale at the root
-    "/",
+    // "/",
 
     // Set a cookie to remember the previous locale for
     // all requests that have a locale prefix
-    `/(ru|en)/:path*`,
+    // `/(ru|en)/:path*`,
 
     // Enable redirects that add missing locales
     // (e.g. `/pathnames` -> `/en/pathnames`)
-    "/((?!_next|_vercel|.*\\..*).*)",
+    // "/((?!_next|_vercel|.*\\..*).*)",
 
     // Disable paths
     // "/((?!api|static|favicon.ico).*)",
-    // "/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest).*)",
   ],
 };
 
