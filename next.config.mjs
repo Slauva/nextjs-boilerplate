6aed1cd55ad0302c9dc3bde09d4d8835a205e91d
Slauva <koshman2015@list.ru>
2024-09-03 16:40:34 +0300

    ci: remove logs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import withNextIntl from "next-intl/plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone", // Docker container optimization
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
 };
 
 export default withNextIntl("./src/shared/lib/i18n.ts")(nextConfig);
